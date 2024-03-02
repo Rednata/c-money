0c2a571623019034a186ac7144d52460af1578bd
Rednata <maknatosh@gmail.com>
2024-03-02 20:33:26 +0700

    feat: choice sort UI

--- src/components/Accounts/Accounts.tsx
+++ src/components/Accounts/Accounts.tsx
@@ -10,11 +10,34 @@ import { accountAddAsync } from '../../store/accountsStore/accountAsyncAction';
 
 export const Accounts = () => {
   const [isVisible, setIsvisible] = useState(true);
-
   const listAccounts = useAppSelector(state => state.accounts.accounts);
-
   const dispatch = useAppDispatch();
 
+  // Логика для отображения сорта
+  const [currentSort, setCurrentSort] = useState('По дате');
+  const [isActivebtnSort, setisActivebtnSort] = useState(false);
+  const commonSortList = [
+    'По дате', 'По балансу', 'По карте', 'По дате транзакции'
+  ];
+  const [sortList, setSortList] = useState(
+    commonSortList.filter(item => item !== currentSort));
+
+  const sortHandleClick = () => {
+    setisActivebtnSort(!isActivebtnSort);
+  };
+
+  const handleChoiceSort = (e: React.MouseEvent<HTMLLIElement>) => {
+    if (e.currentTarget.textContent) {
+      setCurrentSort(e.currentTarget.textContent);
+      setisActivebtnSort(!isActivebtnSort);
+    }
+  };
+
+  useEffect(() => {
+    setSortList(commonSortList.filter(item => item !== currentSort));
+  }, [currentSort]);
+
+  // Добавление нового счета
   const handleClick = () => {
     dispatch(accountAddAsync());
   };
@@ -38,7 +61,7 @@ export const Accounts = () => {
           <Container>
             <div className={style.wrapAccounts}>
               <div className={style.wrapTitle}>
-                <p className={style.title}>Здравствуйте, Александр!</p>
+                <p className={style.title}>Здравствуйте, Guest!</p>
                 <Button
                   cn='btnAccounts'
                   text='Открыть новый счет'
@@ -50,17 +73,48 @@ export const Accounts = () => {
                 <h2 className={style.titleAccounts}>Мои счета</h2>
                 <div className={style.wrapSort}>
                   <p className={style.titleSort}>Сортировка:</p>
-                  <select className={style.select} name="" id="">
-                    <option className={style.option} value="">По дате</option>
+                  <div className={style.wrapSortList}>
+                    <button className={isActivebtnSort ?
+                      style.activeBtnSort : style.btnSort}
+                    onClick={sortHandleClick}>
+                      {currentSort} &#9660;
+                    </button>
+                    {isActivebtnSort && (
+                      <ul className={style.sortList} >
+                        {sortList.map(item => (
+                          <li className={style.sortItem}
+                            onClick={handleChoiceSort}
+                            key={Math.random().toString(16).slice(2, 8)}
+                          >{item}</li>
+                        ))}
+                        {/* <li className={style.sortItem}
+                          onClick={handleChoiceSort}
+                        >По балансу</li>
+                        <li className={style.sortItem}
+                          onClick={handleChoiceSort}
+                        >По номеру</li>
+                        <li className={style.sortItem}
+                          onClick={handleChoiceSort}
+                        >По дате транзакции</li> */}
+                      </ul>
+                    )}
+                  </div>
+
+                  {/* <select className={style.select} name="" id="">
+                    <option
+                      className={style.option} value=""
+                    >
+                      По дате открытия
+                    </option>
                     <option
                       className={style.option} value="">По балансу</option>
                     <option className={style.option} value="">
                       По номеру счета
                     </option>
                     <option className={style.option} value="">
-                      По транзакции
+                      По дате транзакции
                     </option>
-                  </select>
+                  </select> */}
                 </div>
               </div>
 
