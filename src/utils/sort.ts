965fffa7b9ed0b173622a9264c20024afac4cf85
Rednata <maknatosh@gmail.com>
2024-03-15 09:43:01 +0700

    feat: add sort for last transaction`s date

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -16,6 +16,29 @@ export const funcSort = (valueSort: string, listAccounts: any) => {
       console.log();
       return (b.date) >= (a.date) ? -1 : 1;
     });
+  } else {
+    for (let i = 0; i < copyListAccounts.length; i++) {
+      for (let j = i + 1; j < copyListAccounts.length; j++) {
+        if (copyListAccounts[i].transactions.length === 0 &&
+          copyListAccounts[j].transactions.length === 0
+        ) {
+          continue;
+        } else if (copyListAccounts[i].transactions.length === 0) {
+          const temp = copyListAccounts[j];
+          copyListAccounts[j] = { ...copyListAccounts[i] };
+          copyListAccounts[i] = { ...temp };
+        } else if (copyListAccounts[j].transactions.length === 0) {
+          const temp = copyListAccounts[j];
+          copyListAccounts[j] = { ...copyListAccounts[i] };
+          copyListAccounts[i] = { ...temp };
+        } else if (new Date(copyListAccounts[i].transactions[0].date) >
+          new Date(copyListAccounts[j].transactions[0].date)) {
+          const temp = copyListAccounts[j];
+          copyListAccounts[j] = { ...copyListAccounts[i] };
+          copyListAccounts[i] = { ...temp };
+        }
+      }
+    }
   }
   return copyListAccounts;
 };
